29b00f804ff95dcd5096c0cc50f4e4109e8d0510
federazionefantacazz <federazionefantacazz@gmail.com>
2026-06-07 05:21:22 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'fantalega-pwa-v2.22.4'; // Ho messo v2 per forzare l'aggiornamento
+const CACHE_NAME = 'fantalega-pwa-v2.22.5'; // Ho messo v2 per forzare l'aggiornamento
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
